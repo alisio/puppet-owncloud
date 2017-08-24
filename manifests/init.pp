7a07459cd13f80b1e3f5b62abbc051a24fc530b6
Alisio Meneses <alisio@MacBook-Pro-2.local>
2017-08-23 21:05:30 -0300

    Remocao de arquivo desnecessario. Insercao de informacao sobre licensa GPL

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,17 @@
 #
 # Antonio Alisio de Menses Cordeiro <alisio.meneses@gmail.com>
 #
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # References:
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-owncloud-on-centos-7
